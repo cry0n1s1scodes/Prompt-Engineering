3394c466bec99b2835764530ae4e8d9a17378e9b
cry0n1s1scodes <tjanardhan@cherrycreekschools.org>
2025-05-08 15:30:08 -0600

    added automatic replicator

---
+++ Forking/Clone Tools/clone.bat
@@ -0,0 +1,11 @@
+@echo off
+color b
+echo u/Cry0n1s1scodes
+echo Automatic Prompt Enginneering Repository Cloner
+echo Make sure you have git installed on your system, otherwise this may not work!
+pause
+git clone https://github.com/cry0n1s1scodes/Prompt-Engineering.git
+cd Prompt-Engineering
+echo Successfully Cloned the Repository to your machine ✅
+echo Contributors:
+echo Cry0n1s1scodes
